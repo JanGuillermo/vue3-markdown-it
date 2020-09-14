f350e9a8eac120cda9b9f2f940108350aaa54e32
Jan <6843446+JanGuillermo@users.noreply.github.com>
2020-09-14 00:31:23 -0700

    :wrench: Add production-ready library (#5)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const install = (Vue) => {
   Vue.component(VueMarkdownIt.name, VueMarkdownIt);
 };
 
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
 VueMarkdownIt.install = install;
 
 export default VueMarkdownIt;
