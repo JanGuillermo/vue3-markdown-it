c1516cb974f844443c9459659c3374b27f1baf16
JanGuillermo <guillermo.janf+github@gmail.com>
2020-09-14 19:47:36 -0700

    :hammer: Use Vue 3's Composition API for component

--- src/vue-markdown-it.js
+++ src/vue-markdown-it.js
@@ -17,76 +17,74 @@ import MarkdownItSub from 'markdown-it-sub';
 import MarkdownItSup from 'markdown-it-sup';
 import MarkdownItTasklists from 'markdown-it-task-lists';
 import MarkdownItTOC from 'markdown-it-toc-done-right';
+import { onMounted, onUpdated, ref } from 'vue';
+
+const props = {
+  anchor: {
+    type: Object,
+    default: () => ({})
+  },
+  breaks: {
+    type: Boolean,
+    default: false
+  },
+  emoji: {
+    type: Object,
+    default: () => ({})
+  },
+  highlight: {
+    type: Object,
+    default: () => ({})
+  },
+  html: {
+    type: Boolean,
+    default: false
+  },
+  langPrefix: {
+    type: String,
+    default: 'language-'
+  },
+  quotes: {
+    type: String,
+    default: '“”‘’'
+  },
+  source: {
+    type: String,
+    default: ''
+  },
+  tasklists: {
+    type: Object,
+    default: () => ({})
+  },
+  toc: {
+    type: Object,
+    default: () => ({})
+  },
+  typographer: {
+    type: Boolean,
+    default: false
+  },
+  xhtmlOut: {
+    type: Boolean,
+    default: false
+  }
+};
 
 export default {
   name: 'vue3-markdown-it',
   template: '<div ref="md" />',
-  props: {
-    anchor: {
-      type: Object,
-      default: () => ({})
-    },
-    breaks: {
-      type: Boolean,
-      default: false
-    },
-    emoji: {
-      type: Object,
-      default: () => ({})
-    },
-    highlight: {
-      type: Object,
-      default: () => ({})
-    },
-    html: {
-      type: Boolean,
-      default: false
-    },
-    langPrefix: {
-      type: String,
-      default: 'language-'
-    },
-    quotes: {
-      type: String,
-      default: '“”‘’'
-    },
-    source: {
-      type: String,
-      default: ''
-    },
-    tasklists: {
-      type: Object,
-      default: () => ({})
-    },
-    toc: {
-      type: Object,
-      default: () => ({})
-    },
-    typographer: {
-      type: Boolean,
-      default: false
-    },
-    xhtmlOut: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      md: null
-    };
-  },
-  mounted() {
-    this.renderMarkdown();
-  },
-  updated() {
-    this.renderMarkdown();
-  },
-  methods: {
-    renderMarkdown() {
-      this.md = new MarkdownIt();
+  props,
+  setup() {
+    // State
+    const md = ref();
 
-      this.md
+    // Hooks
+    onMounted(() => renderMarkdown());
+    onUpdated(() => renderMarkdown());
+
+    // Methods
+    const renderMarkdown = () => {
+      let markdown = new MarkdownIt()
         .use(MarkdownItAbbr)
         .use(MarkdownItAnchor, this.anchor)
         .use(MarkdownItDeflist)
@@ -100,18 +98,22 @@ export default {
         .use(MarkdownItSub)
         .use(MarkdownItSup)
         .use(MarkdownItTasklists, this.tasklists)
-        .use(MarkdownItTOC, this.toc);
+        .use(MarkdownItTOC, this.toc)
+        .set({
+          breaks: this.breaks,
+          html: this.html,
+          langPrefix: this.langPrefix,
+          quotes: this.quotes,
+          typographer: this.typographer,
+          xhtmlOut: this.xhtmlOut
+        });
 
-      this.md.set({
-        breaks: this.breaks,
-        html: this.html,
-        langPrefix: this.langPrefix,
-        quotes: this.quotes,
-        typographer: this.typographer,
-        xhtmlOut: this.xhtmlOut
-      });
+      md.value = markdown.render(dedent(this.source));
+    };
 
-      this.$refs.md.innerHTML = this.md.render(dedent(this.source));
-    }
+    return {
+      md,
+      renderMarkdown
+    };
   }
 };
