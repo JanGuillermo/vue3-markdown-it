1b2e56c621291e1f8c8301cf54d10d7b0e1bb61a
JanGuillermo <guillermo.janf+github@gmail.com>
2020-09-14 20:52:03 -0700

    :wrench: Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,11 @@ module.exports = {
   },
   plugins: ['prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-
     'prettier/prettier': ['error']
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/__tests__/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
