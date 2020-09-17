d07efb82d0c67592d82bb51c329ee9c68974e0e3
JanGuillermo <guillermo.janf+github@gmail.com>
2020-09-16 20:22:24 -0700

    :zap: Add fork me component

--- src/packs/App.vue
+++ src/packs/App.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
+ForkMe
 Header
 Markdown
 </template>
 
 <script>
+import ForkMe from '@/components/ForkMe';
 import Header from '@/components/Header';
 import Markdown from '@/components/Markdown';
 
 export default {
   components: {
+    ForkMe,
     Header,
     Markdown
   },
