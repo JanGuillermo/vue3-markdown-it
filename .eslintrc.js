e3136b1d7c4508f40a1b72805e0d6a83c7c5372c
Jan <guillermo.janf+github@gmail.com>
2022-01-14 00:35:54 -0800

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   plugins: ['prettier'],
   rules: {
